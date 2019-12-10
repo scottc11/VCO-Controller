c02474454545ae8997029c2b781d0b63e9c40897
Scott Campbell <scottcampbell@MacBooks-MacBook-Pro.local>
2019-12-09 20:16:51 -0500

    added event btn

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "main.h"
 #include "BeatClock.h"
 
+DigitalOut eventLed(PB_5);
+DigitalIn button(PA_7);
 Ticker ticker;
 Timer timer;
 InterruptIn extClockInput(PB_10);
@@ -9,6 +11,8 @@ BeatClock bClock(LOOP_STEP_LED_PIN, LOOP_START_LED_PIN);
 int newClockPeriod;
 int oldClockPeriod;
 int clockPeriod;
+int newButtonState;
+int oldButtonState;
 
 void tick() {
   bClock.tick();
@@ -18,7 +22,7 @@ void extTick() {
   oldClockPeriod = newClockPeriod;
   newClockPeriod = timer.read_us();
   clockPeriod = newClockPeriod - oldClockPeriod;
-  ticker.attach_us(&tick, clockPeriod / PPQ);
+  ticker.attach_us(&tick, clockPeriod / PPQ);  // potentially write this as a flag and update in main loop
 }
 
 int main() {
@@ -30,7 +34,21 @@ int main() {
   extClockInput.rise(&extTick);
   
   while(1) {
-    wait(2);
+    newButtonState = button.read();
+    if (newButtonState != oldButtonState) {
+            // BUTTON PRESSED
+      if (newButtonState == HIGH) {
+        eventLed.write(HIGH);
+      }
+
+      // BUTTON RELEASED
+      else if (newButtonState == LOW) {     
+        eventLed.write(LOW);
+      }
+
+      oldButtonState = newButtonState;
+      wait_ms(5); // debounce
+    }
   }
 }
 
